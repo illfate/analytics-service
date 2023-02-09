480e01996d284c4a2c8d2d5e89f781e7325f117e
hontarau <ilya.hontarau@itechart-group.com>
2023-02-09 13:45:21 +0100

    Add test run in tasks

--- internal/analytics/service_test.go
+++ internal/analytics/service_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"testing"
+	"time"
 
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
@@ -18,25 +19,25 @@ func TestServiceCreateEvents(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	event := analytics.Event{
-		ClientTime: "time",
+		ClientTime: time.Now(),
 	}
 	t.Run("happy path", func(t *testing.T) {
 		repo := mock.NewMockRepository(ctrl)
 
-		repo.EXPECT().InsertEvents(gomock.Any(), event).Return(nil)
+		repo.EXPECT().InsertEvents(gomock.Any(), []analytics.Event{event}).Return(nil)
 
 		service := analytics.NewService(repo)
-		err := service.CreateEvents(context.Background(), event)
+		err := service.CreateEvents(context.Background(), []analytics.Event{event})
 		assert.NoError(t, err)
 	})
 
 	t.Run("error", func(t *testing.T) {
 		repo := mock.NewMockRepository(ctrl)
 
-		repo.EXPECT().InsertEvents(gomock.Any(), event).Return(io.EOF)
+		repo.EXPECT().InsertEvents(gomock.Any(), []analytics.Event{event}).Return(io.EOF)
 
 		service := analytics.NewService(repo)
-		err := service.CreateEvents(context.Background(), event)
+		err := service.CreateEvents(context.Background(), []analytics.Event{event})
 		require.NotNil(t, err)
 		assert.Equal(t, fmt.Errorf("failed to insert events: %w", io.EOF), err)
 	})
--- internal/tests/integration_test.go
+++ internal/tests/integration_test.go
@@ -1,3 +1,5 @@
+//go:build integration_tests
+
 package tests
 
 import (
